e9468650a7231a66952300af04fe5aad78dd397c
praj <pritishraj.official@gmail.com>
2026-01-20 18:55:08 +0530

    Commit 20-01-2026

--- src/DSA/canPlaceFlower.java
+++ src/DSA/canPlaceFlower.java
@@ -10,24 +10,8 @@ public class canPlaceFlower {
         System.out.println(value);
     }
 
+
     public static boolean canPlaceFlowers(int[] flowerbed, int n) {
-        //You can code here
-        for(int i=0;i< flowerbed.length; i++){
-            int left = (i==0) ? 0 : flowerbed[i]-1;
-            int right = (i == flowerbed.length-1) ? 0 : flowerbed[i]+1;
-
-            if(flowerbed[i] == 0 && left == 0 && right == 0){
-                flowerbed[i] = 1;
-                n--;
-                if(n==0){
-                    return true;
-                }
-            }
-        }
-        return n<=0;
-    }
-
-    public boolean canPlaceFlowers1(int[] flowerbed, int n) {
         for (int i = 0; i < flowerbed.length; i++) {
             // Check if we can plant at position i
             int left = (i == 0) ? 0 : flowerbed[i-1];
