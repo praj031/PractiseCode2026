86d08809230152a3db68c5e2b63ec4a0cb362426
praj <pritishraj.official@gmail.com>
2026-04-19 22:49:53 +0530

    DSA

---
+++ src/DSA/DSA_6_19_April.java
@@ -0,0 +1,36 @@
+package DSA;
+
+public class DSA_6_19_April {
+
+    public static void main(String[] args) {
+
+        int[] arr = {-4,-1,0,3,10};
+        int n = arr.length;
+        int left = 0;
+        int right = n-1;
+        int pos = n-1;
+        int[] result = new int[n];
+
+        while (left<=right){
+
+            int leftSqr = arr[left] * arr[left];
+            int rightSqr = arr[right] * arr[right];
+
+            if(leftSqr > rightSqr){
+                result[pos] = leftSqr;
+                left++;
+            }else {
+                result[pos] = rightSqr;
+                right --;
+            }
+            pos--;
+        }
+
+        for(int i=0;i<result.length;i++){
+            System.out.print(result[i]+" ");
+        }
+
+
+    }
+
+}
