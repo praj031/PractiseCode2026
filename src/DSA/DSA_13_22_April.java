e3b909c4b25f851fcde3611fba1fd4cedfa41954
praj <pritishraj.official@gmail.com>
2026-04-24 12:10:19 +0530

    DSA

---
+++ src/DSA/DSA_13_22_April.java
@@ -0,0 +1,35 @@
+package DSA;
+
+public class DSA_13_22_April {
+
+    //is palindrome
+
+    public static void main(String[] args) {
+
+        String s = "A man, a plan, a canal: Panama";
+
+        int left = 0;
+        int right = s.length() - 1;
+
+        while(left < right){
+
+            while(left < right && !Character.isLetterOrDigit(s.charAt(left))){
+                left++;
+            }
+            while(left < right && !Character.isLetterOrDigit(s.charAt(right))){
+                right--;
+            }
+
+            if(Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))){
+                System.out.println("Not Palindrome");
+            }
+
+            left++;
+            right--;
+
+        }
+        System.out.println("Palindrome");
+
+    }
+
+}
---
+++ src/DSA/DSA_14_23_April.java
@@ -0,0 +1,31 @@
+package DSA;
+
+import java.util.Locale;
+
+public class DSA_14_23_April {
+
+    public static void main(String[] args) {
+
+        //Frequency of the character in the string
+        String str = "loveleetcode";
+
+        int[] freq = new int[26]; //Signifies the total letter in the alphabet.
+
+        String lowerString = str.toLowerCase(Locale.of(str)); //to convert the given string in lowercase, to be constant behavior.
+
+        for(int i=0;i<lowerString.length();i++){
+            char ch = lowerString.charAt(i);
+            freq[ch - 'a']++;   //This is how we get the frequency
+        }
+
+        for(int i=0;i<lowerString.length();i++){
+            if(freq[i] > 0){
+                System.out.println((char) (i+'a')+" "+freq[i]);
+            }
+        }
+
+
+
+    }
+
+}
