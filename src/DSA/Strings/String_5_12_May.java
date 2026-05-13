cd61efea9ffc2a2a5ecb8662089059c62f5a11ae
praj <pritishraj.official@gmail.com>
2026-05-13 15:05:00 +0530

    DSA

--- src/DSA/Strings/String_5_12_May.java
+++ src/DSA/Strings/String_5_12_May.java
@@ -6,9 +6,33 @@ public class String_5_12_May {
 
         String str = "A man, a plan, a canal: Panama";
 
+//        String withoutComma = str.replace(",","");
+//        String withoutSpace = withoutComma.replace(" ","");
+//        String withoutCollen = withoutSpace.replace(":","");
+//        String FinalCheckString = withoutCollen.toLowerCase();
+
+        String FinalCheckString = str.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
 
 
+        int left = 0;
+        int right = FinalCheckString.length() - 1;
 
+        char[] arr = FinalCheckString.toCharArray();
+        char temp;
 
+        while(left < right){
+            temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+        String reversed = new String(arr);
+
+        if(FinalCheckString.equals(reversed)){
+            System.out.println("Palindrome");
+        }else {
+            System.out.println("Not Palindrome");
+        }
     }
 }
