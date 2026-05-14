2adb9bf05ea9ae77380e3a1dfe0ab48cc88a7006
praj <pritishraj.official@gmail.com>
2026-05-14 21:23:24 +0530

    DSA

--- src/DSA/DSA_18_14_May.java
+++ src/DSA/DSA_18_14_May.java
@@ -7,15 +7,35 @@ public class DSA_18_14_May {
     public static void main(String[] args) {
         //General way fo checking the 3rd largest element of the array. -
         //By sorting the array into ascending order and printing the last 3rd element of the array.
+        //This will fail in when the elements are duplicate, so we need to add another stream call of distinct
         int[] arr = {12,4,6,1,7,99,4,6,33,99,124,79};
-        int[] sortedArray = Arrays.stream(arr).sorted().toArray();
+        int[] sortedArray = Arrays.stream(arr).sorted().distinct().toArray();
+        for(int m=0;m<sortedArray.length;m++){
+            System.out.print(sortedArray[m] + " ");
+        }
         int length = sortedArray.length - 3;
         int element = sortedArray[length];
         System.out.println("The 3rd largest element in an array = "+element);
 
         //Second approach for doing this is
+        int first = Integer.MIN_VALUE;
+        int second = Integer.MIN_VALUE;
+        int third = Integer.MIN_VALUE;
+        System.out.println(first + " " + second + " " + third);
         for(int i=0;i<arr.length;i++){
-
-       }
+            int num = arr[i];
+            if(num > first ){
+                third = second;
+                second = first;
+                first = num;
+            } else if (num > second && num != first) {
+                third = second;
+                second = num;
+            }
+            else if (num > third && num != second){
+                third = num;
+            }
+        }
+        System.out.println("The largest number = "+third);
     }
 }
