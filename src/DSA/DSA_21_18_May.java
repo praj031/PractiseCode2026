78e013c8ca73e955070b985e8dc542e99a79dac2
praj <pritishraj.official@gmail.com>
2026-05-18 19:51:29 +0530

    DSA

---
+++ src/DSA/DSA_21_18_May.java
@@ -0,0 +1,18 @@
+package DSA;
+
+import java.util.Arrays;
+
+public class DSA_21_18_May {
+    //Square and sort the array
+    public static void main(String[] args) {
+        int[] arr = {-4,-1,0,34,11,77,99};
+
+        int[] result = Arrays.stream(arr)
+                .map(s -> s*s)
+                .sorted()
+                .toArray();
+        for (int i=0;i< result.length;i++){
+            System.out.print(result[i] + " ");
+        }
+    }
+}
