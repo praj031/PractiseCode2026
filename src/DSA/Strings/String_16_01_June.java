7bc4b286b256e8f531566951a78a80c4de63f915
praj <pritishraj.official@gmail.com>
2026-06-01 19:02:20 +0530

    DSA

---
+++ src/DSA/Strings/String_16_01_June.java
@@ -0,0 +1,56 @@
+package DSA.Strings;
+
+public class String_16_01_June {
+
+    //Faulty keyboard
+    //Input = "String"  && Output = "rtsng"
+    public static void main(String[] args) {
+
+        //This will only work in case of single presence of 'i'.
+        String str = "String";
+        String[] arr = str.split("i");
+        char[] charArr = arr[0].toCharArray();
+        String secondString = arr[1];
+
+        for(int i=0;i<charArr.length;i++){
+            int first = 0;
+            int last = charArr.length-1;
+            char temp = charArr[first];
+            charArr[first] = charArr[last];
+            charArr[last] = temp;
+        }
+
+        String revString = new String(charArr);
+
+        String finalString = revString + secondString;
+        System.out.println(finalString);
+
+
+
+    }
+
+}
+
+/*
+Your laptop keyboard is faulty, and whenever you type a character 'i' on it, it reverses the string that you have written.
+Typing other characters works as expected.
+
+You are given a 0-indexed string s, and you type each character of s using your faulty keyboard.
+
+Return the final string that will be present on your laptop screen.
+
+
+
+Example 1:
+
+Input: s = "string"
+Output: "rtsng"
+Explanation:
+After typing first character, the text on the screen is "s".
+After the second character, the text is "st".
+After the third character, the text is "str".
+Since the fourth character is an 'i', the text gets reversed and becomes "rts".
+After the fifth character, the text is "rtsn".
+After the sixth character, the text is "rtsng".
+Therefore, we return "rtsng".
+ */
