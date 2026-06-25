d7cb6cdc0495efb0ce3ee8ca3330b582bf4081af
praj <pritishraj.official@gmail.com>
2026-06-25 21:52:59 +0530

    DSA

---
+++ src/DSA/Strings/String_28_25_June.java
@@ -0,0 +1,31 @@
+package DSA.Strings;
+
+import java.util.Stack;
+
+public interface String_28_25_June {
+
+    //125. Valid Palindrome
+    public static void main(String[] args) {
+
+        String string = "A man, a plan, a canal: Panama";
+        boolean output = isPalindrome(string);
+        System.out.println("Output = "+output);
+
+    }
+
+    static boolean isPalindrome(String string){
+
+        String cleaned = string.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        char[] ch = cleaned.toCharArray();
+        Stack<Character> stack = new Stack<>();
+        for(int i=0;i<cleaned.length();i++){
+            stack.push(ch[i]);
+        }
+        StringBuilder reversed = new StringBuilder();
+        while (!stack.isEmpty()) {
+            reversed.append(stack.pop());
+        }
+        return cleaned.equals(reversed.toString());
+    }
+
+}
