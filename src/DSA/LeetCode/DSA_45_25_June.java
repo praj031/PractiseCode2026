eaa4735225ef7176c961976adfcef8d1a744f2a4
praj <pritishraj.official@gmail.com>
2026-06-25 21:37:17 +0530

    DSA

--- src/DSA/LeetCode/DSA_45_25_June.java
+++ src/DSA/LeetCode/DSA_45_25_June.java
@@ -8,6 +8,11 @@ public class DSA_45_25_June {
         int k = 2;
         int result = maxSumSubArray(array,k);
         System.out.println("Output = "+result);
+
+        int[] array2 = {1,12,-5,-6,50,3};
+        int k2 = 4;
+        double result2 = findMaxAverage(array2,k2);
+        System.out.println("Output = "+result2);
     }
 
     public static int maxSumSubArray(int[] arr, int k){
@@ -34,4 +39,23 @@ public class DSA_45_25_June {
 
     }
 
+    public static double findMaxAverage(int[] arr, int k) {
+        long sum = 0;
+        for (int i = 0; i < k; i++) {
+            sum += arr[i];
+        }
+        long maxSum = sum;
+        int low = 0;
+        int high = k - 1;
+        while (high < arr.length - 1) {
+            sum -= arr[low];
+            low++;
+            high++;
+            sum += arr[high];
+            maxSum = Math.max(maxSum, sum);
+        }
+        return (double) maxSum / k;
+    }
 }
+
+
