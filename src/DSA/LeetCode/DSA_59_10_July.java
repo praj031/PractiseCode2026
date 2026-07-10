0830b17087d1018218ab212509b3a0ecca641292
praj <pritishraj.official@gmail.com>
2026-07-10 18:51:27 +0530

    DSA

---
+++ src/DSA/LeetCode/DSA_59_10_July.java
@@ -0,0 +1,29 @@
+package DSA.LeetCode;
+
+public class DSA_59_10_July {
+    //Longest
+    public static void main(String[] args) {
+        int[] array = {1,1,1,0,0,0,1,1,1,1,0,};
+        int zeroAllowed = 2;
+        int maxLength = longestOnes(array,zeroAllowed);
+        System.out.println("Max Length = "+maxLength);
+    }
+    public static int longestOnes(int[] nums, int k) {
+        int low = 0;
+        int maxLength = 0;
+        int zeroCount = 0;
+        for(int high = 0; high < nums.length; high ++){
+            if(nums[high] == 0){
+                zeroCount++;
+            }
+            while (zeroCount > k){
+                if(nums[low] == 0){
+                    zeroCount --;
+                }
+                low ++;
+            }
+            maxLength = Math.max(maxLength, high - low + 1);
+        }
+        return maxLength;
+    }
+}
